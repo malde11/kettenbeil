fdba2116547d2e7ba903aaaeff70736b615d4ceb
malde <malt.ketten@gmail.com>
2025-01-25 14:25:50 +0100

    video is working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+"use strict";
 
   /* Burger menu toggle nav */
   const topNav      = document.getElementById("myTopnav");
@@ -8,5 +9,24 @@ window.onload = function() {
     topNav.classList.toggle("responsive");
     burgerMenu.classList.toggle("toggleMenu");
   });
+  
 
+  /* Video on screen size
+  const mainVideo = document.getElementById("myVideo");
+        
+  const medi      = "/videos/ground_540p.mp4";
+  const large     = "/videos/ground_1080p.mp4";
+
+      switch ( true ) {
+
+          case window.innerWidth >= 1080:
+              mainVideo.append("<source type='video/mp4' src='" + large + "' />");
+              break;
+
+          case window.innerWidth >= 720:
+              mainVideo.append("<source type='video/mp4' src='" + medi + "' />");
+              break;
+      
+      }
+*/
 };
