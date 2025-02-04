0c744cb642d09a345ec53640765b0fbd732e8a55
malde <malt.ketten@gmail.com>
2025-02-04 23:16:41 +0100

    typo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
 "use strict";
 
-/* Navbar active state */
 window.addEventListener('DOMContentLoaded', ()=> {
-  
-  const activeLink = document.querySelector(".nav-link[href='" + window.location.pathname + "']");
-
-  if(activeLink) activeLink.classList.add("active");
 
   /* Hide Preloader */
   document.getElementById("loader-background").style.display = 'none';
   document.getElementById("loader").style.display = 'none';
+  
+  /* Navbar active state */
+  const activeLink = document.querySelector(".nav-link[href='" + window.location.pathname + "']");
+
+  if(activeLink) activeLink.classList.add("active");
 
   /* Burger-Menu toggle nav */
   const topNav      = document.getElementById("myTopnav");
@@ -19,4 +19,5 @@ window.addEventListener('DOMContentLoaded', ()=> {
     topNav.classList.toggle("responsive");
     burgerMenu.classList.toggle("toggleMenu");
   });
+  
 });
