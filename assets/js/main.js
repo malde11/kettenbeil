727b3c926575e2eadab082409d589031fe93f41f
malde <malt.ketten@gmail.com>
2025-02-04 22:53:30 +0100

    some typo

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,24 +2,21 @@
 
 /* Navbar active state */
 window.addEventListener('DOMContentLoaded', ()=> {
-// Grab the .nav-link whose href is the pathname
-let activeLink = document.querySelector(".nav-link[href='" + window.location.pathname + "']");
-// Set it to active. You want the if condition on the off chance there's 
-// not a viable match.
-if(activeLink) activeLink.classList.add("active");
+  
+  const activeLink = document.querySelector(".nav-link[href='" + window.location.pathname + "']");
+
+  if(activeLink) activeLink.classList.add("active");
 
   /* Hide Preloader */
   document.getElementById("loader-background").style.display = 'none';
   document.getElementById("loader").style.display = 'none';
 
-    /* Burger-Menu toggle nav */
-    const topNav      = document.getElementById("myTopnav");
-    const burgerMenu  = document.getElementById("burgerMenu");
+  /* Burger-Menu toggle nav */
+  const topNav      = document.getElementById("myTopnav");
+  const burgerMenu  = document.getElementById("burgerMenu");
     
-    burgerMenu.addEventListener("click", () => { 
-      topNav.classList.toggle("responsive");
-      burgerMenu.classList.toggle("toggleMenu");
-    });
-
-
+  burgerMenu.addEventListener("click", () => { 
+    topNav.classList.toggle("responsive");
+    burgerMenu.classList.toggle("toggleMenu");
+  });
 });
