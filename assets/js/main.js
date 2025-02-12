3c436709a9c4c4a164d93cb018e1520d295ce0b3
malde <malt.ketten@gmail.com>
2025-02-12 18:27:24 +0100

    still gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,19 +26,37 @@ addEventListener('DOMContentLoaded', ()=> {
     
   });
 
+
   /* Image Gallery */
-    const imgs = document.getElementsByClassName("test").onclick = function() {
+  document.getElementById("myModal").style.display = 'block';
+  document.getElementById("myModal").style.display = "none";
 
-      const expandImg = document.getElementById("expandedImg");
-      const imgText = document.getElementById("imgtext");
-      
-      expandImg.src = imgs.src;
-      imgText.innerHTML = imgs.alt;
-      expandImg.parentElement.style.display = "block";
-  };
-
-  const galleryBtn = document.getElementsByClassName("closebtn").onclick = function() {
-
-    galleryBtn.parentElement.style.display='none';
-  };
+  let slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
 });
