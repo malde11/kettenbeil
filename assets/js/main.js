a13a2df5754a256926e8a85bf615954bae8ddbb1
malde <malt.ketten@gmail.com>
2025-02-25 18:15:15 +0100

    cleaning code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,26 @@
 "use strict";
 
-addEventListener('load', ()=> {
-
-    /* Hide Preloader */
-    document.getElementById("loader-background").style.display = 'none';
-    document.getElementById("loader").style.display = 'none';
-
+addEventListener("load", () => {
+  /* Hide Preloader */
+  document.getElementById("loader-background").style.display = "none";
+  document.getElementById("loader").style.display = "none";
 });
 
-
-addEventListener('DOMContentLoaded', ()=> {
-  
+addEventListener("DOMContentLoaded", () => {
   /* Navbar active state */
-  const activeLink = document.querySelector(".nav-link[href='" + window.location.pathname + "']");
-  if(activeLink) activeLink.classList.add("active");
-
+  const activeLink = document.querySelector(
+    ".nav-link[href='" + window.location.pathname + "']"
+  );
+  if (activeLink) activeLink.classList.add("active");
 
   /* Burger-Menu toggle nav */
-  const topNav      = document.getElementById("myTopnav");
-  const burgerMenu  = document.getElementById("burgerMenu");
-    
-  burgerMenu.addEventListener("click", () => { 
+  const topNav = document.getElementById("myTopnav");
+  const burgerMenu = document.getElementById("burgerMenu");
+
+  burgerMenu.addEventListener("click", () => {
     topNav.classList.toggle("responsive");
     burgerMenu.classList.toggle("toggleMenu");
-    
   });
-
 
   /* Image Gallery */
   /*
