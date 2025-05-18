afcee2b5c161308362830b28f4ef7d9690ab021e
Malte Mac <malt.ketten@gmail.com>
2025-05-18 20:05:58 +0200

    only  first gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,95 +35,65 @@ addEventListener("DOMContentLoaded", () => {
     document.getElementById("myModal").style.display = "none";
   }
 
-  const image = document.querySelector(".test-id");
-  console.log(image);
-  if(image) {
-    image.addEventListener("click", openModal, false);
-  }
-
-  const close = document.getElementsByClassName("close")[0];
-  if(close) {
-    close.addEventListener("click", closeModal, false);
-  }
-
-
-
-
-
-
-
   var slideIndex = 1;
   showSlides(slideIndex);
 
-
   // Next/previous controls
   function plusSlides(n) {
-    showSlides((slideIndex += n));
+    showSlides(slideIndex += n);
   }
-
-  const prev = document.querySelector(".prev");
-  if(prev) {
-    prev.addEventListener("click", function() {
-      plusSlides(-1);
-    });
-  };
-
-  const next = document.querySelector(".next");
-  if(next) {
-    next.addEventListener("click", function() {
-      plusSlides(1);
-    });
-  };
-  
 
   // Thumbnail image controls
-  // Get the current slide
-  const currentSlide = document.querySelector("demo");
-  if(currentSlide) {
-    for (let i = 0; i < currentSlide.length; i++) {
-      currentSlide[i].addEventListener("click", function() {
-        currentSlide(i + 1);
-      });
-    }
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
   }
 
-  // Show the slides
   function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("demo");
     var captionText = document.getElementById("caption");
-
-    // If the user clicks on the image, open the modal
-    // and show the current slide
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
-
-    
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-
-    // Hide all slides
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
-
-    // Hide all dots
     for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
     }
-
-    // Show the current slide
-    // and add an "active" class to the thumbnail image
-    // that is clicked
-    // Show the current slide
-    // and add an "active" class to the thumbnail image
-    // that is clicked
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
   }
+
+  const galleryimages = document.getElementsByClassName("galleryimage")
+  for (let i=0; i<galleryimages.length; i++) {
+      galleryimages[i].addEventListener("click", (e) => {
+          openModal()
+          currentSlide(parseInt(e.target.dataset.id))
+      })
+  }
+
+  const miniimages = document.getElementsByClassName("demo")
+  for (let i=0; i<miniimages.length; i++) {
+      miniimages[i].addEventListener("click", (e) => {
+          currentSlide(parseInt(e.target.dataset.id))
+      })
+  }
+
+  const closebutton = document.getElementsByClassName("close")
+  closebutton[0].addEventListener("click", (e) => {
+      closeModal()
+  })
+
+  const previousbutton = document.getElementsByClassName("prev")
+  previousbutton[0].addEventListener("click", (e) => {
+      plusSlides(-1)
+  })
+
+  const nextbutton = document.getElementsByClassName("next")
+  nextbutton[0].addEventListener("click", (e) => {
+      plusSlides(1)
+  })
 
 });
