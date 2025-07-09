24280eb692557a115ec0f978894ba4ed46bb9593
Malte Mac <malt.ketten@gmail.com>
2025-07-09 17:49:37 +0200

    scroll to top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,14 +27,14 @@ addEventListener("DOMContentLoaded", () => {
 
 
   // Scroll to top button
-  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
-  const rootElement = document.documentElement;
-  scrollToTopBtn.addEventListener("click", () => {
-    rootElement.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  });
+  // Get the element
+  let topBtn = document.querySelector(".top-btn");
+
+  // On Click, Scroll to the page's top, replace 'smooth' with 'instant' if you don't want smooth scrolling
+  topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+
+  // On scroll, Show/Hide the btn with animation
+  window.onscroll = () => topBtn.style.opacity = window.scrollY > 500 ? 1 : 0;
 
   
 
