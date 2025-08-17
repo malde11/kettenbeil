c029d58978fb1cacfbae89e21236b8c677371061
Malte Mac <malt.ketten@gmail.com>
2025-08-17 13:13:16 +0200

    testing recap

--- assets/js/main.js
+++ assets/js/main.js
@@ -427,8 +427,7 @@ addEventListener("DOMContentLoaded", () => {
 
   if (position.length > 0) {
     // Add class to the parent div of the iframe
-    position[2].parentElement.setAttribute('id', 'g-active'); 
-
+    position[0].parentElement.classList.add("g-active");
   }
 
 });
