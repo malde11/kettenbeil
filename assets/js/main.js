3cdcb8c1f872a7c6a6df9f02fad0f024fbf465c9
Malte Mac <malt.ketten@gmail.com>
2025-08-17 16:56:36 +0200

    normal

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,7 +41,7 @@ addEventListener("DOMContentLoaded", () => {
   console.log(captcha);
   // If the captcha element exists, set its data-size attribute
   if (captcha.length > 0) {
-    captcha[0].setAttribute("data-size", "compact");
+    captcha[0].setAttribute("data-size", "compact-normal");
   }
 
 
